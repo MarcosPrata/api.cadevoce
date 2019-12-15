5c33c49f11b87cb4c15f0d9df2d0456af6cb7297
MarcosPrata <mhpjunior3@gmail.com>
2019-12-15 14:20:23 -0300

    1.0

---
+++ models/banco.php
@@ -0,0 +1,134 @@
+<?php
+class MongoHelper
+{
+    public $URI = 'mongodb://heroku_23x9xstl:fbv156gdenqi4b7hls9gducn6v@ds353338.mlab.com:53338/heroku_23x9xstl?retryWrites=false';
+    public $nome = 'heroku_23x9xstl';
+    public $TABELA_USUARIOS = 'USUARIOS';
+    public $TABELA_GRUPOS = 'GRUPOS';
+    function db()
+    {
+        return new MongoDB\Driver\Manager($this->URI);
+    }
+    function colecao($collection_name)
+    {
+        return new MongoDB\Collection($this->db(), $this->nome, $collection_name);
+    }
+}
+
+class Usuario
+{
+    public $nome;
+    public $email;
+    public $grupos;
+    public $localizacao;
+
+    function __construct()
+    {
+        $this->nome = "";
+        $this->email = "";
+        $this->grupos = [];
+        $this->localizacao = ['lat' => '', 'lon' => ''];
+    }
+
+    function __construct1(ArrayObject $json)
+    {
+        $this->nome = ($json['nome'] == null) ? '' : $json['nome'];
+        $this->email = ($json['email'] == null) ? '' : $json['email'];
+        $this->grupos = ($json['grupos'] == null) ? [] : $json['grupos'];
+        $this->localizacao = ($json['localizacao'] == null) ? ['lat' => '', 'lon' => ''] : $json['localizacao'];
+    }
+
+    function toJSON()
+    {
+        return [
+            'nome' => $this->nome,
+            'email' => $this->email,
+            'grupos' => $this->grupos,
+            'localizacao' => $this->localizacao
+        ];
+    }
+}
+
+class UsuariosBD
+{
+    function insert(Usuario $usuario)
+    {
+        $banco = new MongoHelper();
+        $usuarios = $banco->colecao($banco->TABELA_USUARIOS);
+        $insertOneResult = $usuarios->insertOne($usuario->toJSON());
+        if ($insertOneResult->getInsertedCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function getUsuario($email)
+    {
+        $banco = new MongoHelper();
+        $usuarios = $banco->colecao($banco->TABELA_USUARIOS);
+        $usuario = new Usuario($usuarios->findOne(['email'=>$email]));
+        return $usuario;
+    }
+    function alterarLocalizacao($email, $lat, $lon)
+    {
+        $banco = new MongoHelper();
+        $usuarios = $banco->colecao($banco->TABELA_USUARIOS);
+        return $usuarios->findOneAndUpdate(
+            ['email'=>$email],['$SET=>',[
+                'localizacao' =>['lat' => $lat, 'lon' => $lon]
+            ]]
+        );
+    }
+}
+
+class Grupo
+{
+    public $nome;
+    public $senha;
+    public $usuarios;
+
+    function __construct()
+    {
+        $this->nome = "";
+        $this->senha = "";
+        $this->usuarios = [];
+    }
+
+    function __construct1(ArrayObject $json)
+    {
+        $this->nome = ($json['nome'] == null) ? '' : $json['nome'];
+        $this->senha = ($json['senha'] == null) ? '' : $json['senha'];
+        $this->usuarios = ($json['usuarios'] == null) ? [] : $json['usuarios'];
+    }
+
+    function toJSON()
+    {
+        return [
+            'nome' => $this->nome,
+            'senha' => $this->senha,
+            'usuarios' => $this->usuarios
+        ];
+    }
+}
+
+class GruposBD
+{
+    function insert(Usuario $usuario)
+    {
+        $banco = new MongoHelper();
+        $grupos = $banco->colecao($banco->TABELA_GRUPOS);
+        $insertOneResult = $grupos->insertOne($usuario->toJSON());
+        if ($insertOneResult->getInsertedCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function getGrupo($nome)
+    {
+        $banco = new MongoHelper();
+        $grupos = $banco->colecao($banco->TABELA_GRUPOS);
+        $grupo = new Grupo($grupos->findOne(['nome'=>$nome]));
+        return $grupo;
+    }
+}
---
+++ rest/usuarios.php
@@ -0,0 +1,32 @@
+<?php
+require '../vendor/autoload.php';
+require '../models/banco.php';
+header('Access-Control-Allow-Origin: *');
+
+$funcao = $_GET['funcao'];
+switch ($funcao) {
+    case 'sync_data':
+        $json = json_decode(file_get_contents('php://input'), true);
+        $email = $json['email'];
+        $latitude = $json['lat'];
+        $longitude = $json['lon'];
+
+        $BDusuarios = (new UsuariosBD());
+        $BDgrupos = (new GruposBD());
+
+        $BDusuarios->alterarLocalizacao($email,$latitude,$longitude);
+        $usuario = $BDusuarios->getUsuario($email);
+        $grupos = [];
+        foreach($usuario->grupos as $grupo){
+            $grupo = $BDgrupos->getGrupo($grupo);
+            $participantes = [];
+            foreach($grupo->usuarios as $participante){
+                $participantes[] = $BDusuarios->getUsuario($participante);
+            }
+            $grupos[] = [$grupo->nome => $participantes];
+
+        }
+        echo json_encode(['error'=>false, 'grupos' => $grupos]); 
+    break;
+}
+?>
